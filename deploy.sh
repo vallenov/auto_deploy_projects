1c6cfef9d3217626f26b63ffedc7bacc0b4350c6
vallenov <a2lenov@mail.ru>
2022-04-14 12:04:10 +0300

    Добавлено описание

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-PROJECT_DIR=/home/vladimir/prog/python/projects
+#Обязательно добавить все проекты в список безопасных репозиториев
+#sudo git config --global --add safe.directory /home/vladimir/prog/python/projects/name_of_repo
+
+PROJECTS_DIR=/home/vladimir/prog/python/projects
 
 #Имена проекта и демона могут отличаться, главное - соблюдать порядок
 PROJECT_NAME_LIST=(TBot MessageSender system-monitor)
 DAEMON_NAME_LIST=(TBot MailSender system_monitor)
 
-DL=$PROJECT_DIR/deploy.log
+DL=$PROJECTS_DIR/deploy.log
 
 function log {
     echo `date` - $1 >> $DL
@@ -16,7 +19,7 @@ function deploy_project {
 if [[ $# == 2 ]]
 then
     log "Check update for $1"
-    cd $PROJECT_DIR/$1
+    cd $PROJECTS_DIR/$1
     IS_UPDATE=`git pull`
     if [[ $IS_UPDATE == "Already up to date." ]]
     then
